9a8a87a9d2ba3cf0622c26a4a56096767654497f
roberto.leite <roberto.leite@DW10SSATX28.ssatx.local>
2021-07-04 16:44:09 -0300

    TESTE

---
+++ Construtores_EX1/src/DETRAN/Carro.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DETRAN;
+
+/**
+ *
+ * @author roberto.leite
+ */
+public class Carro {
+    
+    String modelo, fabricante, anoFabricacao, cor, ultimaVistoria, nomeMotorista;
+    int chassi, cnhMotorista, numDocumento;
+    boolean licenciado;
+
+    public Carro(String modelo, String fabricante, String anoFabricacao, String cor, String nomeMotorista, int chassi, int cnhMotorista) {
+        this.modelo = modelo;
+        this.fabricante = fabricante;
+        this.anoFabricacao = anoFabricacao;
+        this.cor = cor;
+        this.nomeMotorista = nomeMotorista;
+        this.chassi = chassi;
+        this.cnhMotorista = cnhMotorista;
+    }
+    
+    
+    public void vistoria(boolean opVistoria){
+        
+    }
+    
+    public void licenciamento(boolean isLicenciado){
+    
+    }
+    
+    public void transferenciaPropriedade(String nomeMotorista, int chassi, int cnhMotorista, int numDocumento){
+        
+    }
+    
+}
